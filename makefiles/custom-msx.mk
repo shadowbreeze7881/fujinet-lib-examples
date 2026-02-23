c049f250cd3e6f5e64963ad72f316bb3e26e85dc
shadowbreeze7881 <theretrolife@soulexi.com>
2026-02-22 20:55:09 -0500

    Update custom-msx.mk
    Initial

--- makefiles/custom-msx.mk
+++ makefiles/custom-msx.mk
@@ -1,1 +1,10 @@
+# COMPILE FLAGS
 
+################################################################
+# DISK creation
+
+SUFFIX = .com
+DISK_TASKS += .create-dsk
+#VICE_HOME := $(dir $(shell which xvic))
+
+DISK_FILE = $(DIST_DIR)/$(PROGRAM).dsk
