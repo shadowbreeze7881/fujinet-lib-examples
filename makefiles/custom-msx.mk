c07f2209d3ad0fefb2e04bb6e573f82f8674a714
shadowbreeze7881 <theretrolife@soulexi.com>
2026-02-22 21:09:17 -0500

    Update custom-msx.mk
    Update the file with the script to create an MSX disk for the program.

--- makefiles/custom-msx.mk
+++ makefiles/custom-msx.mk
@@ -1,3 +1,5 @@
+# I don't know which one we are going to use, the disk version or the rom
+
 # COMPILE FLAGS
 
 ################################################################
@@ -8,3 +10,18 @@ DISK_TASKS += .create-dsk
 #VICE_HOME := $(dir $(shell which xvic))
 
 DISK_FILE = $(DIST_DIR)/$(PROGRAM).dsk
+
+.create-dsk:
+	@which dsktool > /dev/null 2>&1 ; \
+	if [ $$? -eq 0 ] ; then \
+		echo "creating $(DISK_FILE) from program $(PROGRAM_TGT)" ; \
+		if [ -f "$(DISK_FILE)" ] ; then \
+			rm "$(DISK_FILE)" ; \
+		fi ; \
+    dsktool C 720 $(DISK_FILE) ; \
+    dsktool A $(DISK_FILE) $(DIST_DIR)/$(PROGRAM_TGT)$(SUFFIX) ; \
+		rm $(DIST_DIR)/$(PROGRAM_TGT)$(SUFFIX)  ; \
+	else \
+		echo -e "\nERROR! You must install dsktool from https://github.com/nataliapc/MSX_devs/tree/master/dsktool to create msx disks\n" ; \
+		exit 1 ; \
+	fi
