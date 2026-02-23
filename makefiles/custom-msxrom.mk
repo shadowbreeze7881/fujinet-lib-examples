45f181a70b8e04d50a04cd10149714053f5d4459
shadowbreeze7881 <theretrolife@soulexi.com>
2026-02-23 03:11:27 -0500

    Update custom-msxrom.mk
    comment out the create-rom part. I don't think i need it for now.

--- makefiles/custom-msxrom.mk
+++ makefiles/custom-msxrom.mk
@@ -12,16 +12,16 @@ ROM_TASKS += .create-rom
 ROM_FILE = $(DIST_DIR)/$(PROGRAM).rom
 
 .create-rom:
-	@which dsktool > /dev/null 2>&1 ; \
-	if [ $$? -eq 0 ] ; then \
-		echo "creating $(DISK_FILE) from program $(PROGRAM_TGT)" ; \
-		if [ -f "$(DISK_FILE)" ] ; then \
-			rm "$(DISK_FILE)" ; \
-		fi ; \
-	    dsktool C 720 $(DISK_FILE) ; \
-	    dsktool A $(DISK_FILE) $(DIST_DIR)/$(PROGRAM_TGT)$(SUFFIX) ; \
-		rm $(DIST_DIR)/$(PROGRAM_TGT)$(SUFFIX)  ; \
-	else \
-		echo -e "\nERROR! You must install dsktool from https://github.com/nataliapc/MSX_devs/tree/master/dsktool to create msx disks\n" ; \
-		exit 1 ; \
-	fi
+#	@which dsktool > /dev/null 2>&1 ; \
+#	if [ $$? -eq 0 ] ; then \
+#		echo "creating $(DISK_FILE) from program $(PROGRAM_TGT)" ; \
+#		if [ -f "$(DISK_FILE)" ] ; then \
+#			rm "$(DISK_FILE)" ; \
+#		fi ; \
+#	    dsktool C 720 $(DISK_FILE) ; \
+#	    dsktool A $(DISK_FILE) $(DIST_DIR)/$(PROGRAM_TGT)$(SUFFIX) ; \
+#		rm $(DIST_DIR)/$(PROGRAM_TGT)$(SUFFIX)  ; \
+#	else \
+#		echo -e "\nERROR! You must install dsktool from https://github.com/nataliapc/MSX_devs/tree/master/dsktool to create msx disks\n" ; \
+#		exit 1 ; \
+#	fi
